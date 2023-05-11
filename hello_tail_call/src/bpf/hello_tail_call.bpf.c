40484f3a924dfd4ee9012da89b4ebd76b62b5ecc
meowjesty <meowjesty_nyan@outlook.com>
2023-05-11 00:53:34 -0300

    Better understanding of bpf_tail_call and tracepoint_args + raw_tracepoint.

--- hello_tail_call/src/bpf/hello_tail_call.bpf.c
+++ hello_tail_call/src/bpf/hello_tail_call.bpf.c
@@ -19,33 +19,60 @@ typedef struct {
   __uint(type, BPF_MAP_TYPE_PROG_ARRAY);
   __uint(max_entries, 256 * 1024);
   __uint(key_size, sizeof(__u32));
-  __uint(value_size, sizeof(__u32));
 
+  /// These will be our syscall opcodes.
+  __uint(value_size, sizeof(__u32));
 } ProgramArray;
 
 SEC(".maps")
 ProgramArray syscalls;
 
-SEC("tracepoint/syscalls/sys_enter_execve")
+/// Helpful links:
+///
+/// https://mozillazg.com/2022/05/ebpf-libbpf-raw-tracepoint-common-questions-en.html#hidformat-of-sec-content
+///
+/// The more general syscall entry point. Represents any of the `sys_enter_*`
+/// calls, such as `sys_enter_excve` that we were using.
+///
+/// For the full list of the events that raw tracepoints can monitor:
+///
+///```sh
+/// sudo cat /sys/kernel/debug/tracing/available_events
+///```
+SEC("raw_tracepoint/sys_enter")
 int run(struct bpf_raw_tracepoint_args *tracepoint_args) {
+  // We access the `bpf_raw_tracepoint_args[1]` which contains the syscall id.
+  //
+  // `bpf_raw_tracepoints[0]` contains the regs (see `pt_regs`), which are the
+  // arguments to the corresponding syscall (for example the
+  // `const char *pathname, char *const argv[], char *const envp[]`, from
+  // `execve`).
   int opcode = tracepoint_args->args[1];
 
-  // TODO(alex) [high] 2023-05-08: Is this enough to move the tail call
-  // forwards? Are we accessing a valid index?
+  // Jumps into another bpf program (indexed here by `opcode`) preserving the
+  // stack frame. Sort of like a loop, it should never come back from this
+  // (doesn't go back to a previous program).
+  //
+  // On failure it's basically skipped, and we move on to the `bpf_trace_printk`
+  // line (continues execution of the current program).
   bpf_tail_call(tracepoint_args, &syscalls, opcode);
 
+  // We only display something here if the `bpf_tail_call` fails, due to
+  // `opcode` not being a valid index for the `syscalls` map.
   bpf_trace_printk("syscall: {%d}", opcode);
-
   return 0;
 }
 
-SEC("tc")
+/// Loaded into the `syscalls` program array map, and is executed when the
+/// `bpf_tail_call` `opcode` is `execve`.
+SEC("raw_tracepoint")
 int enter_execve(void *ctx) {
   bpf_trace_printk("enter_execve %d", 0);
   return 0;
 }
 
-SEC("tc")
+/// Loaded into the `syscalls` program array map.
+SEC("raw_tracepoint")
 int timer(struct bpf_raw_tracepoint_args *tracepoint_args) {
   if (tracepoint_args->args[1] == 222) {
     bpf_trace_printk("create timer %d", 222);
@@ -58,5 +85,7 @@ int timer(struct bpf_raw_tracepoint_args *tracepoint_args) {
   return 0;
 }
 
-SEC("tc")
+/// Used for syscalls that we want to do nothing (it's a `bpf_tail_call`
+/// program).
+SEC("raw_tracepoint")
 int ignore_opcode(void *ctx) { return 0; }
--- hello_tail_call/src/main.rs
+++ hello_tail_call/src/main.rs
@@ -1,4 +1,4 @@
-use libbpf_rs::PrintLevel;
+use libbpf_rs::{MapFlags, PrintLevel};
 use nix::unistd::Uid;
 use std::{process::exit, thread::sleep, time::Duration};
 
@@ -30,10 +30,110 @@ fn main() {
     let builder = HelloTailCallSkelBuilder::default();
 
     let open = builder.open().unwrap();
+    // TODO(alex) [high] 2023-05-11: Debug in lldb why we crash here!
     let mut skel = open.load().unwrap();
     let _attached = skel.attach().unwrap();
 
-    // TODO(alex) [high] 2023-05-08: Interact with the `run` program.
+    let enter_execve_fd = skel.progs().enter_execve().fd().to_be_bytes();
+    let ignore_fn_fd = skel.progs().ignore_opcode().fd().to_be_bytes();
+    let timer_fn_fd = skel.progs().timer().fd().to_be_bytes();
+
+    let mut maps = skel.maps_mut();
+    let syscalls = maps.syscalls();
+
+    // execve
+    syscalls
+        .update(&59_u64.to_be_bytes(), &enter_execve_fd, MapFlags::empty())
+        .unwrap();
+
+    // calls we care about
+    syscalls
+        .update(&222_u64.to_be_bytes(), &timer_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&223_u64.to_be_bytes(), &timer_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&224_u64.to_be_bytes(), &timer_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&225_u64.to_be_bytes(), &timer_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&226_u64.to_be_bytes(), &timer_fn_fd, MapFlags::empty())
+        .unwrap();
+
+    // ignore these that come up a lot
+    syscalls
+        .update(&21_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&22_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&25_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&29_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&56_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&57_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&63_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&64_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&66_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&72_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&73_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&79_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&98_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&101_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&115_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&131_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&134_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&135_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&139_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&172_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&233_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&280_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+    syscalls
+        .update(&291_u64.to_be_bytes(), &ignore_fn_fd, MapFlags::empty())
+        .unwrap();
+
     loop {
         log::info!("...");
 
