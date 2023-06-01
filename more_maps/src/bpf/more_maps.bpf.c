9e643c547f6ed77014434fce2e2b55011e9298c4
meowjesty <meowjesty_nyan@outlook.com>
2023-06-01 02:18:11 -0300

    Implementing more_maps (incomplete).

--- more_maps/src/bpf/more_maps.bpf.c
+++ more_maps/src/bpf/more_maps.bpf.c
@@ -3,6 +3,8 @@
 /// `bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h`
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
 
 /// `SEC` applies to global items.
 SEC("license")
@@ -57,3 +59,39 @@ Buffer global_buffer;
 
 SEC(".maps")
 UserMessages user_messages;
+
+/// Auto-attaches to whatever arch, a `kprobe` to `execve`.
+///
+/// `BPF_KPROBE_SYSCALL` macro is defined in `<bpf/bpf_tracing.h>`, but it also
+/// requires `<bpf/bpf_core_read.h>` to work.
+///
+/// This macro is defined in`libbpf` and allows us to access the syscall
+/// arguments by name, hence why we can use `pathname` here, instead of the
+/// usual `void *context` (or the concrete type of context).
+///
+/// - `sample_program`: the name of our program;
+/// - `pathname`: argument to `execve`, it's the path of the program that's
+///  going to be executed.
+SEC("ksyscall/excve")
+int BPF_KPROBE_SYSCALL(sample_program, const char *pathname) {
+  Data data = {};
+  UserMessage *message;
+
+  data.pid = bpf_get_current_pid_tgid() >> 32;
+  data.uid = BPF_FUNC_get_current_uid_gid & 0xffffffff;
+
+  bpf_get_current_comm(&data.command, sizeof(data.command));
+  bpf_probe_read_user_str(&data.path, sizeof(data.path), pathname);
+
+  message = bpf_map_lookup_elem(&user_messages, &data.uid);
+
+  if (message) {
+    bpf_probe_read_kernel(&data.message, sizeof(data.message), message->value);
+  } else {
+    bpf_probe_read_kernel(&data.message, sizeof(data.message), message);
+  }
+
+  // TODO(alex) [high] 2023-06-01: Finish this sample.
+
+  return 0;
+}
